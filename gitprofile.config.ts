e888b45bf40ae2e4c324a1c2c2dda98455608ff7
Eliane Maciel <eliane.faveron@gmail.com>
2025-01-23 17:39:04 -0300

    curriculo pdf

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -156,7 +156,7 @@ const CONFIG = {
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      './CV - Eliane Isadora Faveron Maciel.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'Scrum',
